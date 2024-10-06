c3d4c09d76dbffa0825d23003aa144fde09904d2
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-10-05 21:50:21 -0300

    :closed_lock_with_key: Implements password hash using bcriptjs

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const userSchema = z.object({
@@ -11,11 +12,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = userSchema.parse(request.body)
 
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if(userWithSameEmail){
+    return reply.status(409).send()
+  }
+
+  const password_hash = await hash(password, 6)
+
   await prisma.user.create({
     data: {
       email,
       name,
-      password_hash: password,
+      password_hash,
     },
   })
 
