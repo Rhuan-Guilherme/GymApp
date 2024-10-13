20779a95e647dcedc98dec48da76e67e742ecccb
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-10-13 16:05:35 -0300

    :alembic: Config test environments in new databse schema

--- prisma/vitest-environments-prisma/prisma-test-environments.ts
+++ prisma/vitest-environments-prisma/prisma-test-environments.ts
@@ -1,12 +1,40 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import type { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+function generateDatabseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+const prisma = new PrismaClient()
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
-  setup() {
-    console.log('setup')
+  async setup() {
+    const schema = randomUUID()
+    const dataBaseURL = generateDatabseURL(schema)
+
+    process.env.DATABASE_URL = dataBaseURL
+
+    execSync('npx prisma migrate deploy')
+
     return {
-      teardown() {},
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
+      },
     }
   },
 }
